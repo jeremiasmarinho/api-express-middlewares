4e08f11275e75490a529bf2f4402fc537ac58962
Jeremias Marinho <jeremias10@msn.com>
2021-11-12 07:35:13 -0300

    concluindo a aula

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const exp = require("constants");
 const express = require("express");
 const app = express();
 const port = 3000;
 
+const logMiddleware = function (req, res, next) {
+  console.log("API recebeu alguma informação")
+
+  next();
+};
+
+app.use(express.static("./site"));
+app.use(logMiddleware);
+
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.get("/pt", (req, res) => res.send("Olá mundo!") )
 
 app.listen(port, ()=> 
   console.log(`example app listening at http://localhost:${port}`)
